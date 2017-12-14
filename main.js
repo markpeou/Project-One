9ed354b6a09be7572812ef3d5d72ad824656034c
markpeou <markpeou@gmail.com>
2017-12-15 10:06:21 +1100

    make a commit

--- main.js
+++ main.js
@@ -1,57 +1,114 @@
 
-var one = document.getElementById('1')
-var two = document.getElementById('2')
-var three = document.getElementById('3')
-var four = document.getElementById('4')
-var five = document.getElementById('5')
-var six = document.getElementById('6')
-var seven = document.getElementById('7')
-var eight = document.getElementById('8')
-var nine = document.getElementById('9')
+var one = document.getElementById('b1')
+var two = document.getElementById('b2')
+var three = document.getElementById('b3')
+var four = document.getElementById('b4')
+var five = document.getElementById('b5')
+var six = document.getElementById('b6')
+var seven = document.getElementById('b7')
+var eight = document.getElementById('b8')
+var nine = document.getElementById('b9')
 
-var playerOne = []
-var playerTwo = []
 var counter = 0
-var board = ['','','','','','','','','']
-var combo1 = [['0','1','2']];
-var combo2 = [['3','4','5']];
-var combo3 = [['6','7','8']];
-var combo4 = [['0','3','6']];
-var combo5 = [['1','4','7']];
-var combo6 = [['2','5','8']];
-var combo7 = [['0','4','8']];
-var combo8 = [['2','4','6']];
-var winningCombos = combo1.concat(combo2,combo3,combo4,combo5,combo6,combo7,combo8)
-      // turn arrays into a strings
-      // then see if the strings are true or false
 
-var turn = function(event) {
+// var $ = function(selector) {
+//   return document.querySelector(selector)
+// }
+function setMessage(msg){
+  document.getElementById('message').textContent = msg;
+}
+setMessage('X starts')
+  var turn = function(event) {
+
   // if textContent = '' then assign 'x'
   // when textContent is not empty
-
   if (event.target.textContent !== "") {
-    return
+    return setMessage('pick another square');
   }
-counter++
-//changing players
+    counter++
+    //changing players
  if (counter % 2 !== 0) {
-   event.target.textContent = 'X' ;
-   board = event.target.id ='x';
- } else {
-   event.target.textContent = 'O' ;
-   board = ('o')
+    event.target.textContent = 'X' ;
+    board = event.target
+    setMessage("O's Turn")
 
+ } else {
+    event.target.textContent = 'O' ;
+    board = event.target = 'o';
+    setMessage("X's Turn")
+  }
   //record players turns, so put into array
+  checkForWin()
  }
 
- checkForWin()
- //check for wins
- // if box one is = x && box two = x && box three = x
- // playerOne Wins
-}
 
-function checkForWin() {
-}
+ function checkForWin() {
+  if (document.getElementById('b1').textContent === 'X' &&
+      document.getElementById('b2').textContent === 'X' &&
+      document.getElementById('b3').textContent === 'X' ||
+      document.getElementById('b4').textContent === 'X' &&
+      document.getElementById('b5').textContent === 'X' &&
+      document.getElementById('b6').textContent === 'X' ||
+      document.getElementById('b7').textContent === 'X' &&
+      document.getElementById('b8').textContent === 'X' &&
+      document.getElementById('b9').textContent === 'X' ||
+      document.getElementById('b1').textContent === 'X' &&
+      document.getElementById('b4').textContent === 'X' &&
+      document.getElementById('b7').textContent === 'X' ||
+      document.getElementById('b2').textContent === 'X' &&
+      document.getElementById('b5').textContent === 'X' &&
+      document.getElementById('b8').textContent === 'X' ||
+      document.getElementById('b3').textContent === 'X' &&
+      document.getElementById('b6').textContent === 'X' &&
+      document.getElementById('b9').textContent === 'X' ||
+      document.getElementById('b1').textContent === 'X' &&
+      document.getElementById('b5').textContent === 'X' &&
+      document.getElementById('b9').textContent === 'X' ||
+      document.getElementById('b3').textContent === 'X' &&
+      document.getElementById('b5').textContent === 'X' &&
+      document.getElementById('b7').textContent === 'X') {
+        return setMessage('x wins!')
+
+  } else if (
+        document.getElementById('b1').textContent === 'O' &&
+        document.getElementById('b2').textContent === 'O' &&
+        document.getElementById('b3').textContent === 'O' ||
+        document.getElementById('b4').textContent === 'O' &&
+        document.getElementById('b5').textContent === 'O' &&
+        document.getElementById('b6').textContent === 'O' ||
+        document.getElementById('b7').textContent === 'O' &&
+        document.getElementById('b8').textContent === 'O' &&
+        document.getElementById('b9').textContent === 'O' ||
+        document.getElementById('b1').textContent === 'O' &&
+        document.getElementById('b4').textContent === 'O' &&
+        document.getElementById('b7').textContent === 'O' ||
+        document.getElementById('b2').textContent === 'O' &&
+        document.getElementById('b5').textContent === 'O' &&
+        document.getElementById('b8').textContent === 'O' ||
+        document.getElementById('b3').textContent === 'O' &&
+        document.getElementById('b6').textContent === 'O' &&
+        document.getElementById('b9').textContent === 'O' ||
+        document.getElementById('b1').textContent === 'O' &&
+        document.getElementById('b5').textContent === 'O' &&
+        document.getElementById('b9').textContent === 'O' ||
+        document.getElementById('b3').textContent === 'O' &&
+        document.getElementById('b5').textContent === 'O' &&
+        document.getElementById('b7').textContent === 'O') {
+        return setMessage('o wins!')
+      }
+    }
+//
+
+// check if box is full or empty if empty game is still going
+  // if box is filled then game has finished
+
+//     }  else if (
+//     document.querySelectorAll('.sq').textContent != 'O' ||
+//     document.querySelectorAll('.sq').textContent != 'X')
+//         return setMessage('its a draw')
+// }
+
+
 
 
 one.addEventListener('click',turn);
@@ -63,28 +120,3 @@ six.addEventListener('click',turn);
 seven.addEventListener('click',turn);
 eight.addEventListener('click',turn);
 nine.addEventListener('click',turn);
-
-
-// function checkWin () {
-//   // if player has 3 in a row, they win
-//   checkOneDirection(0,1,2);
-//   checkOneDirection(3,4,5);
-//   checkOneDirection(6,7,8);
-//   checkOneDirection(0,3,6);
-//   checkOneDirection(1,4,7);
-//   checkOneDirection(2,5,8);
-//   checkOneDirection(0,4,8);
-//   checkOneDirection(2,4,6);
-// }
-
-
-// function checkOneDirection(first,second,third) {
-// if (document.querySelectorAll(div) === (first))
-
-
-// function checkOneDirection(first,second,third) {
-//   if ($('section div').eq(first).text() === player &&
-//       $('section div').eq(second).text() === player &&
-//       $('section div').eq(third).text() === player)
-//     $('#winner-message').text('player ' + player + ' won!');
-// }
